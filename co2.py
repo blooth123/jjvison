c54d395b3d6435ae47dfc2403cf25a9e4f698201
blooth123 <blooth123@naver.com>
2020-07-10 15:01:15 +0900

    co2

--- co2.py
+++ co2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import sys, serial, time
+import requests, json
+from influxdb import InfluxDBClient as influxdb
 
 comm = '/dev/ttyAMA0'
 baudrate = 38400
@@ -13,8 +15,29 @@ while (True):
         rcvBuf = bytearray()
         device.reset_input_buffer()
         rcvBuf = device.read_until (size=12)
-        print rcvBuf[:-5]
-
+        print rcvBuf
+        temp = rcvBuf.find('p')
+        a = rcvBuf[2:temp]
+        print a
+        
+        data=[{
+            'measurement' : 'co2',
+            'tags' : {
+                'visionUmi' : '2410',
+                },
+            'fields':{
+                'co2' : a,
+                }
+            }]
+        clien = None
+        client = influxdb('localhost', 8086, 'root','root','co2')
+        if client is not None:
+            try:
+                client.write_points(data)
+            except Exception as e:
+                print("Exception read") + str(e)
+            finally:
+                client.close()
     except Exception as e:
         print("Exception read") + str(e)
 
