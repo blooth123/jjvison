a17b3337cadb306e6b9fb8c3145a73a43e2b2a18
blooth123 <blooth123@naver.com>
2020-07-10 12:42:16 +0900

    co2

---
+++ co2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys, serial, time
+
+comm = '/dev/ttyAMA0'
+baudrate = 38400
+
+device = serial.Serial (comm, baudrate, timeout = 5)
+print (device)
+
+while (True):
+    try:
+        rcvBuf = bytearray()
+        device.reset_input_buffer()
+        rcvBuf = device.read_until (size=12)
+        print rcvBuf
+    except Exception as e:
+        print("Exception read") + str(e)
+
+    time.sleep(5)
+
+
+
